855ed9f3799e1eacbc2c9ba7d09480a7b3eaddb7
liyuncong <294265421@qq.com>
2017-09-23 19:49:51 +0800

    清理代码

--- train_word2vec/train_word2vec.py
+++ train_word2vec/train_word2vec.py
@@ -11,17 +11,8 @@ baseDir = r'/home/liyuncong/program/dataset/news/sougo/cs/2012/'
 sentences = word2vec.Text8Corpus(baseDir + u"utf8_news_sohusite_xml.englishlike.txt")
 # 训练skip-gram模型; 默认window=5
 model = word2vec.Word2Vec(sentences, size=200)
-
 # 保存模型
-model.save(baseDir + u"word2vec.model")
-# 对应的加载方式
-model_2 = word2vec.Word2Vec.load(baseDir + u"word2vec.model")
-
-# 以一种C语言可以解析的形式存储词向量
-# model.save_word2vec_format(u"书评.model.bin", binary=True)
-model.wv.save_word2vec_format(baseDir + u"word2vec.model")
-# 对应的加载方式
-model_3 = word2vec.Word2Vec.load_word2vec_format(baseDir + u"word2vec.model", binary=True)
+model.wv.save_word2vec_format(baseDir + u'word2vec.vector')
 
 if __name__ == "__main__":
     pass

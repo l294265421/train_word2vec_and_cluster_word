934cf59b2f46f7490bf6f11871a692954fb3cfa9
liyuncong <294265421@qq.com>
2017-09-23 08:17:48 +0800

    添加readme.md

--- cluster_word/cluster_word_by_kmeans.py
+++ cluster_word/cluster_word_by_kmeans.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 
 baseDir = r'/home/liyuncong/program/dataset/news/sougo/cs/2012/'
 
+# word2vec.vector文件下载地址为http://pan.baidu.com/s/1miLxrOs，该文件是通过train_word2vec.train_word2vec.py训练得到的
 with open(baseDir + "word2vec.vector") as word_vector_file:
     words = []
     vectors = []
--- train_word2vec/train_word2vec.py
+++ train_word2vec/train_word2vec.py
@@ -5,6 +5,9 @@ from gensim.models import word2vec
 baseDir = r'/home/liyuncong/program/dataset/news/sougo/cs/2012/'
 
 # 加载语料
+# utf8_news_sohusite_xml.englishlike.txt是通过搜狗语料库生成的文件，其中，词已经通过空格分开
+# 原始搜狗数据地址为http://www.sogou.com/labs/resource/cs.php，完整版
+# utf8_news_sohusite_xml.englishlike.txt地址为
 sentences = word2vec.Text8Corpus(baseDir + u"utf8_news_sohusite_xml.englishlike.txt")
 # 训练skip-gram模型; 默认window=5
 model = word2vec.Word2Vec(sentences, size=200)
